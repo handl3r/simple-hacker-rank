9ae71cb751494aa71d6d3e3a0fec2a5311ea4f3b
Code55x <thaibuixuan34@gmail.com>
2019-08-28 16:59:12 +0700

    done: RunContainer service

--- app/services/run_container.rb
+++ app/services/run_container.rb
@@ -1,50 +1,37 @@
 # frozen_string_literal: true
 
+# Image for each languge must be built before
+# Class to run a container when server receive code form user be saved
+# call from controller
 class RunContainer
-  # Class to run a container for each request test or submit, call from controller
-  # language -> string : to run container correspond with language request
-  # source -> string : name file to keep source submit (like x_id1.rb not include character /)
-  # id -> integer : identify for user and user's container
-  # result -> name file to get result return (not include character /)
-  def initialize(language, source, result, id)
-    @source = source
-    @result = result
+  SOURCE = 'run_file.rb'
+  DES = 'result.rb'
+  SUBMIT_CODE_RESULT_FOLDER = '/home/thai/www/RailsPR/Project2/submit_code_result'
+  TEMP_CONTAINER_FOLDER = '/app'
+
+  # language -> string: to run corresponded container  with language request
+  # id -> integer : identify for user
+  def initialize(language, id)
     @language = language
-    @name_image = "project2_#{language}_image"
-    @name_container = "project2_#{language}_container" # add _#{id} in the end when dont test
+    @name_image = "#{language}_image"
+    @name_container = "#{language}_container_#{id}"
+    @run_file = "#{language}_#{id}.rb"
+    @result = "result_#{id}.txt"
   end
 
-  # method to copy file keep source submit to container and run
+  # main method to run a container and run code inside it
   def run
-    path_to_submit_folder = '/home/thai/www/RailsPR/Project2/submit_code_result'
-    run_file_container = 'test.rb'
-    result_file_container = 'result.txt'
-    # image was build yet
-    run_container = "docker run -d -it --rm --name #{@name_container} -v #{path_to_submit_folder}:/app   #{@name_image} bash"
+    run_container = "docker run -d -it --rm --name #{@name_container} -v #{SUBMIT_CODE_RESULT_FOLDER}:#{TEMP_CONTAINER_FOLDER}   #{@name_image} bash"
     if system(run_container) == false
       false
     else
-      run_code(run_file_container, result_file_container)
+      run_code(@run_file, @result)
       # then stop container
       system("docker stop #{@name_container}")
     end
   end
 
   private
-
-  # method to copy file from host to container and reverse
-  # source -> string: name source file (not include character /)
-  # des -> string: name destination file (not include character /)
-  # type: 1-> host to container (exactly main container to other container), 2 -> container to host(reverse)
-  def copy_file(source, des, type)
-    copy_file_command = ''
-    if type == 1
-      copy_file_command = "docker cp #{source} #{@name_container}:/app/#{des}"
-    elsif type == 2
-      copy_file_command = "docker cp #{@name_container}:/app/#{source} ../../dockerStorage/#{@language}/#{des}"
-    end
-    system(copy_file_command)
-  end
 
   # method to run code form file on container and save result
   # runfile : string -> name file keep code to run (not include character /)
@@ -54,5 +41,3 @@ class RunContainer
     system(run_code_command)
   end
 end
-
-# system("docker run -d --name ruby_container --rm -it testruby bash")
