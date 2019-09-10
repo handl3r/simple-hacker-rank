656efebf89a2bad2b678b64466a0317f29e5140f
Code55x <thaibuixuan34@gmail.com>
2019-09-10 23:12:17 +0700

    Fix: convention

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, controllers: {sessions: 'users/sessions'}
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
